483e684bd7e3c266195910ed13009ddbf29a969b
unknown <zapatarzg@gmail.com>
2023-06-02 14:39:10 -0600

    Encriptador Final

---
+++ script.js
@@ -0,0 +1,41 @@
+// Función para encriptar el texto
+function encriptar() {
+    var texto = document.getElementById("input_text").value.toLowerCase();
+    // Reemplazar las vocales según los requisitos
+    texto = texto.replace(/e/g, 'enter');
+    texto = texto.replace(/i/g, 'imes');
+    texto = texto.replace(/a/g, 'ai');
+    texto = texto.replace(/o/g, 'ober');
+    texto = texto.replace(/u/g, 'ufat');
+    
+    mostrar_texto(texto)
+  }
+  
+  // Función para desencriptar el texto
+  function desencriptar() {
+    var texto = document.getElementById("input_text").value.toLowerCase();
+    
+    // Reemplazar las palabras encriptadas por las vocales originales
+    texto = texto.replace(/enter/g, 'e');
+    texto = texto.replace(/imes/g, 'i');
+    texto = texto.replace(/ai/g, 'a');
+    texto = texto.replace(/ober/g, 'o');
+    texto = texto.replace(/ufat/g, 'u');
+    
+    mostrar_texto(texto)
+  }
+
+  function mostrar_texto(texto) {
+    document.getElementById("imagen_output").style.display = "none";
+    document.getElementById("ph_text").style.display = "none";
+    document.getElementById("copy_button").style.display = "block";
+    document.getElementById("text_result").innerHTML = texto;
+  }
+  
+
+function copiar() {
+    var contenido = document.getElementById("text_result").value;
+    navigator.clipboard.writeText(contenido);
+    alert("Texto Copiado!")
+}
+      
